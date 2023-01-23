a0ea33baee18a3d69bf87b287f40db0a39d495f1
MarkOdinSon <gladkov.mark.123232@gmail.com>
2023-01-23 19:43:49 +0200

    start icon added but need to do modal window and api for rating

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: { user: 0, admin: 1, super_admin: 2, default: :user }
+
+  private
+
+  def self.user_rated_this_movie?(user_id, movie_id)
+    return true if Rating.where(user_id: user_id, movie_id: movie_id).exists?
+
+    false
+  end
 end
