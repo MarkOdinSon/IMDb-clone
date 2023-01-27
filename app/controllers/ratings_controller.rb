018880d94533a7b53da84ce1098a951cb30571b7
MarkOdinSon <gladkov.mark.123232@gmail.com>
2023-01-27 13:00:15 +0200

    fetch turbo_stream in progress

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,6 @@
 class RatingsController < ApplicationController
   # skip_before_action :verify_authenticity_token (uncomment it in the case if you don't have csrf-token)
+  # [https://fly.io/ruby-dispatch/turbostream-fetch/]
 
   def create
     flash[:alert] = "Create rating"
@@ -19,6 +20,14 @@ class RatingsController < ApplicationController
   def update
     flash[:alert] = "Update rating"
 
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream: turbo_stream.replace("my_turbo_frame_50",
+                                                  render_to_string( partial: 'movies/rating', locals: { movie: Movie.find(50) }) )
+      }
+
+      format.html { redirect_to (request.referrer || root_path) }
+    end
   end
 
   def destroy
