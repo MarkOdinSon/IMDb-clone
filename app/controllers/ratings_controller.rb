208ec572bff7aeb3d22c5900f011bda95cde665f
MarkOdinSon <gladkov.mark.123232@gmail.com>
2023-01-27 14:26:00 +0200

    fetch turbo_stream in progress_2

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -5,12 +5,12 @@ class RatingsController < ApplicationController
   def create
     flash[:alert] = "Create rating"
 
-    Movie.find(50).update(rating: 5)
+    Movie.find(69).update(rating: 5)
 
     respond_to do |format|
       format.turbo_stream {
-        render turbo_stream: turbo_stream.replace("my_turbo_frame_50",
-                                                  render_to_string( partial: 'movies/rating', locals: { movie: Movie.find(50) }) )
+        render turbo_stream: turbo_stream.replace("turbo_frame_show_movie_rating_inf_72",
+                                                  render_to_string( partial: 'movies/rating', locals: { movie: Movie.find(72) }) )
       }
 
       format.html { redirect_to (request.referrer || root_path) }
